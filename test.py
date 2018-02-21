5c0766dd749a85b2d36b6fce0ca0dfebb0ffca07
weilixu <weilix@alumni.cmu.edu>
2018-02-21 10:13:01 -0500

    update model

--- test.py
+++ test.py
@@ -9,15 +9,11 @@ model_key="0dde5a46-4d07-4b99-907f-0cfedf301072"
 #2. define the absolute directory of your energy model
 file_dir = "/Users/weilixu/Desktop/5ZoneAirCooled.idf"
 #3. this is optional
-comment = "new upload 2"
-#4. simulation Type. this is optional
-st = bsh.get_simulation_type()
-st.set_regular()
 
 newSj = bsh.new_simulation_job(model_key)
 #5. start the API call
 # note if fast simulation, call increaseAgents to increase the agent numbers
-response = newSj.create_model(file_dir, comment, st.type, st.agent)
+response = newSj.create_run_model(file_dir)
 if(response == 'success'):
   while newSj.track_simulation():
     print (newSj.trackStatus)
