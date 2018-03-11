def22fc40d8de5c542240be854ea71079dec5852
weilixu <weilix@alumni.cmu.edu>
2018-03-11 19:18:41 -0400

    add cooling and heating

--- BuildSimHubAPI/measures/__init__.py
+++ BuildSimHubAPI/measures/__init__.py
@@ -9,3 +9,8 @@ from .roof_rvalue import RoofRValue
 from .light_lpd import LightLPD
 from .infiltration import Infiltration
 from .occupancy_sensor import OccupancySensor
+from .daylit_sensor import DaylightingSensor
+from .cooling_all_cop import CoolingCOP
+from .cooling_coil_cop import CoolingCoilCOP
+from .cooling_chiller_cop import CoolingChillerCOP
+from .heating_efficiency import HeatingEfficiency
