267500230a12728c34c978299c0dcbc11966668a
weilixu <weilix@alumni.cmu.edu>
2018-03-12 17:07:48 -0400

    update the plots - use plotly

--- plot_test.py
+++ plot_test.py
@@ -1,16 +1,14 @@
 import BuildSimHubAPI as bsh_api
 
 usr_key = '94266512-c5ea-463d-a108-9dd1e84c07ca'
-model_key = '28c2ec3f-fe72-4c8f-a6c1-579fe14297c9'
+model_key = '5dc2b500-7821-4c2a-b27c-266fdbdddb40'
 
 results = bsh_api.helpers.ParametricModel(usr_key, model_key)
-#results = bsh_api.helpers.Model(usr_key, model_key)
 
-#zone_load = results.zone_load('ZN_1_FLR_1_SEC_2')
-#print(zone_load)
 result_dict = results.net_site_eui()
 result_unit = results.lastParameterUnit
 print(result_dict)
 
 plot = bsh_api.postprocess.ParametricPlot(result_dict, result_unit)
-plot.parallel_coordinate_plotly()
+plot.scatter_chart_plotly()
+#plot.parallel_coordinate_plotly('LPD')
