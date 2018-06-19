733bac5d46ba6aba927c017398d78974f5f6263a
weilixu <weilix@alumni.cmu.edu>
2018-06-19 18:29:43 -0400

    hide the model

--- test/test.py
+++ test/test.py
@@ -1,8 +1,8 @@
 import BuildSimHubAPI as bsh_api
 import BuildSimHubAPI.postprocess as pp
 
-project_api_key = "771ec223-482c-4aef-acab-0504735d9e78"
-model_api_key = "17475332-3683-40e1-862d-d758626bc110"
+project_api_key = "771"
+model_api_key = "1747"
 results = bsh_api.helpers.ParametricModel(project_api_key, model_api_key)
 if results:
     # Collect results
