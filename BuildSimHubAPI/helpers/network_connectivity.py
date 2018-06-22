b8d54acef918a3d8f44b251fb9276dcdac7bd1b1
weilix88 <weilix88@gmail.com>
2018-06-22 10:46:55 -0400

    test connectivity

---
+++ BuildSimHubAPI/helpers/network_connectivity.py
@@ -0,0 +1,9 @@
+import socket
+
+def internet(host="8.8.8.8", port=53, timeout=3):   # port 53: DNS
+    try:
+        socket.setdefaulttimeout(timeout)
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+        return True
+    except:
+        return False
