b762e262ac71ffc44f7b69b754baf2ac628d8031
weilix88 <weilix88@gmail.com>
2018-06-24 21:33:10 -0400

    close socket

--- BuildSimHubAPI/helpers/network_connectivity.py
+++ BuildSimHubAPI/helpers/network_connectivity.py
@@ -5,6 +5,7 @@ def internet(host="8.8.8.8", port=53, timeout=3):   # port 53: DNS
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(timeout)
         s.connect((host, port))
+        s.close()
         return True
     except:
         return False
