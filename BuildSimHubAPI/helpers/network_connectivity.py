786a9d407bac1af97eda5a82fb41c88a1ecd2a59
weilix88 <weilix88@gmail.com>
2018-06-25 13:51:53 -0400

    detect networking by trying google

--- BuildSimHubAPI/helpers/network_connectivity.py
+++ BuildSimHubAPI/helpers/network_connectivity.py
@@ -1,7 +1,7 @@
 import socket
 
 
-def internet(host="8.8.8.8", port=53, timeout=3):   # port 53: DNS
+def internet(host="www.google.com", port=80, timeout=5):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(timeout)
