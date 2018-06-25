7171e34b42b4c4fa676473ea3fb066c200ede2ad
weilix88 <weilix88@gmail.com>
2018-06-24 21:31:37 -0400

    set timeout on local socket instance

--- BuildSimHubAPI/helpers/network_connectivity.py
+++ BuildSimHubAPI/helpers/network_connectivity.py
@@ -2,8 +2,9 @@ import socket
 
 def internet(host="8.8.8.8", port=53, timeout=3):   # port 53: DNS
     try:
-        socket.setdefaulttimeout(timeout)
-        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(timeout)
+        s.connect((host, port))
         return True
     except:
         return False
