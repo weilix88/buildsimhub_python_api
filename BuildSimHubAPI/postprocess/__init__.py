8ae57e90a9c2eeff733b964323d7bd28ccbf51f1
weilixu <weilix@alumni.cmu.edu>
2018-08-02 16:04:37 -0400

    post process for monthly data
    Add post process code that convert the extracted monthly data into pandas df

--- BuildSimHubAPI/postprocess/__init__.py
+++ BuildSimHubAPI/postprocess/__init__.py
@@ -6,3 +6,4 @@ from .one_zone_load import OneZoneLoad
 from .hourly_data_plot import HourlyPlot
 from .html_table_plot import HTMLTable
 from .model_list import ModelList
+from .monthly_data import MonthlyTable
