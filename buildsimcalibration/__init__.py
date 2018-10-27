c8af4b538fcfd49500a282e1297536812f0742f9
weilixu <weilix@alumni.cmu.edu>
2018-10-27 16:32:19 -0400

    buildsim calibration
    Integrate BC-STAN scripts from AChong.

---
+++ buildsimcalibration/__init__.py
@@ -0,0 +1,8 @@
+"""
+This package is developed jointly between BuildSim team and Professor Adrian Chong.
+
+The original script and example can be found at GITHUB page: https://github.com/adChong/bc-stan
+
+The paper published can be found at: https://www.sciencedirect.com/science/article/pii/S0378778818307539?via%3Dihub
+
+"""
